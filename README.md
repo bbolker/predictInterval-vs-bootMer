f5db8ba45ccdf3a70ff8de07d1ed0ab731225a5b
Ben Bolker <bbolker@gmail.com>
2024-12-04 18:12:59 -0500

    rearrange and extend examples

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# predictInterval-vs-bootMer
+ # predictInterval-vs-bootMer
+
 Debugging major discordance in uncertainty estimates using predictInterval()
+
+## BMB notes
+
+* `merTools::predictInterval()` and `bootMer` are giving very different answers
+* possible causes:
+    * bad computation of vcov? (RX vs Hessian)
+	* failure of Wald approx?
+	* conditioning on estimates of theta?
+	
--- soib_reprex.R
+++ soib_reprex.R
@@ -1,55 +1,128 @@
 library(dplyr)
+library(tidyr)
 library(lme4)
 library(merTools)
-
+library(glmmTMB)
+library(ggplot2); theme_set(theme_bw())
+par_cores <- 8L
+options("glmmTMB.cores" = par_cores, "glmmTMB.autopar" = FALSE)
 
 load("reprex_pred_lwdu.RData")
-# subsetting the data, still reproduces issue
-data_lwdu <- slice_sample(data_lwdu, n = 100000) 
-
+## subsetting the data, still reproduces issue
+set.seed(101) ## going to subsample data randomly ...
+n_sub <- 1e4
+n_boot <- 100
+data_lwdu <- slice_sample(data_lwdu, n = n_sub)
 
 # model
-model <- glmer("OBSERVATION.COUNT ~ month + month:log(no.sp) + timegroups + (1|gridg3/gridg1)", 
-               data = data_lwdu, family = binomial(link = 'cloglog'), 
-               nAGQ = 0, control = glmerControl(optimizer = "bobyqa"))
+t_lme4 <- system.time(
+    model_lme4 <- glmer(OBSERVATION.COUNT ~ month + month:log(no.sp) + timegroups +
+                       (1|gridg3/gridg1), 
+                   data = data_lwdu, family = binomial(link = 'cloglog'), 
+                   nAGQ = 0, control = glmerControl(optimizer = "bobyqa"))
+)
 
-# dataframe to predict
-data_to_pred <- data_lwdu %>% 
-  distinct(month, timegroups) %>% 
+## convert glmer call to glmmTMB (eliminate unused/unneeded args)
+##  (consider profile = TRUE to match nAGQ=0 ?)
+cc <- getCall(model)
+cc[[1]] <- quote(glmmTMB)
+cc$nAGQ <- cc$control <- NULL
+t_glmmTMB <- system.time(
+    model_glmmTMB <- eval(cc)
+)
+
+## equivalent to nAGQ=0
+t_glmmTMB.prof <- system.time(
+    model_glmmTMB.prof <- update(model_glmmTMB,
+                                 control = glmmTMBControl(profile = TRUE))
+)
+
+data_to_pred <- data_lwdu |>
+  distinct(month, timegroups) |>
   mutate(no.sp = 15,
          gridg1 = data_lwdu$gridg1[1], 
-         gridg3 = data_lwdu$gridg3[1])
+         gridg3 = data_lwdu$gridg3[1]) |>
+    as.data.frame()  ## avoid predictInterval warning
 
-
-# bootMer
-
-pred_fun <- function(input_model) {
-  predict(input_model, newdata = data_to_pred, re.form = NA, allow.new.levels = TRUE)
-  # not specifying type = "response" because will later transform prediction along with SE
+pred_fun <- function(input_model, ret_val = c("vector", "list"))  {
+    ret_val <- match.arg(ret_val)
+    p0 <- predict(input_model, newdata = data_to_pred, re.form = NA,
+                  allow.new.levels = TRUE)
+    out <- c(getME(input_model, c("theta", "fixef")),
+             list(pred = p0))
+    if (ret_val == "vector") out <- unlist(out)
+    return(out)
 }
 
+skel <- pred_fun(model_lme4, ret_val = "list")
+results <- relist(pred_fun(model_lme4), skeleton = skel)
+
 # tictoc::tic("bootMer 10 sims")
-pred_bootMer <- bootMer(model, 
-                        nsim = 10, # for faster compute, estimate doesn't change much with high sims
+t_boot <- system.time(
+    pred_bootMer <- bootMer(model_lme4, 
+                        nsim = n_boot,
                         FUN = pred_fun, 
-                        seed = 1000, use.u = FALSE, type = "parametric", 
-                        parallel = "no", ncpus = par_cores)
+                        seed = 1000,
+                        use.u = FALSE, type = "parametric", 
+                        parallel = "yes", ncpus = par_cores)
+)
 
-bootmer_mean <- median(na.omit(pred_bootMer$t[,1]))
-bootmer_se <- sd(na.omit(pred_bootMer$t[,1]))
-# tictoc::toc()
+preds <- t(apply(pred_bootMer$t, 1,
+      \(x) {
+          r <- relist(x, skeleton =  skel)
+          r$pred
+      }))
+
+compare_intervals <- function(model, newdata = NULL, level = 0.8,
+                              nboot = 100, seed = NULL) {
+    if (!is.null(seed)) set.seed(seed)
+    pi <- predictInterval(model, newdata = newdata,
+                          type = "linear.prediction", level = level)
+    bb <- bootMer(model, nsim = nboot, FUN = predict,
+                  seed = 1000, use.u = FALSE, type = "parametric",
+                  parallel = "yes", ncpus = par_cores)
+    bb2 <- bootMer(model, nsim = nboot, FUN = predict,
+                  seed = 1000, use.u = TRUE, type = "parametric",
+                  parallel = "yes", ncpus = par_cores)
+    p2 <- predict(model, newdata = newdata, se.fit = TRUE,
+                  re.form = NA)
+    levs <- c((1-level)/2, (1+level)/2)
+    qci_wid <- \(x) diff(quantile(x, levs))
+    c(
+        ## mean CI width
+        predictInterval = with(pi, mean(upr-lwr)),
+        bootMer = mean(apply(bb$t, 2, qci_wid)),
+        bootMer.useu = mean(apply(bb2$t, 2, qci_wid)),
+        predict.se =  mean(p2$se.fit*diff(qnorm(levs)))
+    )
+}
+
+## try this for a "known-good" example
+set.seed(101)
+sim_data <- expand.grid(rep = 1:20, g = factor(1:20))
+sim_data$x <- rnorm(nrow(sim_data))
+sim_data$y <- simulate(~ 1 + x + (1 + x|g),
+                       newdata = sim_data,
+                       family = binomial(link = "cloglog"),
+                       newparams = list(theta = c(1,0,1),
+                                        beta = c(0, 1)))[[1]]
+
+model_sim <- glmer(y ~ 1 + x + (1 + x|g),
+                 data = sim_data,
+                 family = binomial(link = "cloglog"))
+
+## predictIntervals doesn't work with newdata = NULL
+compare_intervals(model_sim, newdata = sim_data, seed = 101)
+
+## untested binomial GLMM warning
+## approximation warning from predict.merMod
+
+## predictInterval         bootMer      predict.se 
+##       2.9255899       2.4023305       0.7104698 
+
+model_sim2 <- lmer(Reaction ~ 1 + Days + (Days | Subject), sleepstudy)
+compare_intervals(model_sim2, newdata = sleepstudy, seed = 101)
+## predictInterval         bootMer      predict.se 
+##        75.66799        95.18719        24.90286 
 
 
-# predictInterval
-
-# tictoc::tic("predictInterval")
-model_predint = predictInterval(model, newdata = data_to_pred, which = "fixed",
-                                level = 0.48, type = "linear.prediction")
-predint_mean <- model_predint$fit
-predint_se <- model_predint$fit - model_predint$lwr
-# tictoc::toc()
-
-
-# print comparisons
-paste("Means: bootMer =", bootmer_mean, ", predictInterval =", predint_mean[1])
-paste("SEs: bootMer =", bootmer_se, ", predictInterval =", predint_se[1])
