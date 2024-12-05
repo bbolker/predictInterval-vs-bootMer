e44f9bbb38a966bad10e7a65757ef5a425744e50
Ben Bolker <bbolker@gmail.com>
2024-12-04 20:59:51 -0500

    reorganizing

---
+++ Makefile
@@ -0,0 +1,5 @@
+soib_reprex_orig.Rout: soib_reprex_orig.R
+
+soib_summary.html: soib_reprex_orig.Rout soib_summary.rmd
+
+include makestuff/wrapR.mk
--- soib_reprex_orig.R
+++ soib_reprex_orig.R
@@ -1,10 +1,13 @@
 library(dplyr)
 library(lme4)
 library(merTools)
-
+n_boot <- 100
+par_cores <- 8
+clev <- 0.48  ## why???
 
 load("reprex_pred_lwdu.RData")
-# subsetting the data, still reproduces issue
+## subsetting the data, still reproduces issue
+set.seed(101)
 data_lwdu <- slice_sample(data_lwdu, n = 100000) 
 
 
@@ -30,10 +33,10 @@ pred_fun <- function(input_model) {
 
 # tictoc::tic("bootMer 10 sims")
 pred_bootMer <- bootMer(model, 
-                        nsim = 10, # for faster compute, estimate doesn't change much with high sims
+                        nsim = n_boot, # for faster compute, estimate doesn't change much with high sims
                         FUN = pred_fun, 
                         seed = 1000, use.u = FALSE, type = "parametric", 
-                        parallel = "no", ncpus = par_cores)
+                        parallel = "multicore", ncpus = par_cores)
 
 bootmer_mean <- median(na.omit(pred_bootMer$t[,1]))
 bootmer_se <- sd(na.omit(pred_bootMer$t[,1]))
@@ -44,12 +47,26 @@ bootmer_se <- sd(na.omit(pred_bootMer$t[,1]))
 
 # tictoc::tic("predictInterval")
 model_predint = predictInterval(model, newdata = data_to_pred, which = "fixed",
-                                level = 0.48, type = "linear.prediction")
+                                level = clev, type = "linear.prediction")
 predint_mean <- model_predint$fit
 predint_se <- model_predint$fit - model_predint$lwr
 # tictoc::toc()
 
+model_predfit = predict(model, newdata = data_to_pred,
+                        se.fit = TRUE)
+
+
+levs <- c((1-clev)/2, (1+clev)/2)
+qq <- qnorm(levs)
+model_predmat = with(model_predfit,
+                     data.frame(fit = fit,
+                                lwr = fit + qq[1]*se.fit,
+                                upr = fit + qq[2]*se.fit))
 
 # print comparisons
 paste("Means: bootMer =", bootmer_mean, ", predictInterval =", predint_mean[1])
 paste("SEs: bootMer =", bootmer_se, ", predictInterval =", predint_se[1])
+
+save(list = c("pred_bootMer", "model_predint", "model_predmat",
+              "predint_mean", "bootmer_mean", "predint_se", "bootmer_se"),
+     file = "soib_reprex_orig.rda")
